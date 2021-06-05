a193245c42018d80d02fbc11d3f50c801fe6a317
jiefeng <lowjiefeng1998@gmail.com>
2021-06-06 01:37:09 +0800

    test(Tag): add test cases for Tag model

---
+++ server/test/models/tag_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class TagTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:user_1)
+  end 
+
+  test "valid_tag with name and user" do
+    @tag = Tag.new(name: "First Time!")
+    @tag.user = @user
+    assert @tag.valid?
+  end
+
+  test "invalid tag with empty name" do
+    @tag = Tag.new(name: "")
+    @tag.user = @user
+    assert @tag.invalid?
+  end
+
+  test "invalid tag with nil name" do
+    @tag = Tag.new(name: nil)
+    @tag.user = @user
+    assert @tag.invalid?
+  end
+
+  test "invalid tag without user" do
+    @tag = Tag.new(name: "Hello World!")
+    assert @tag.invalid?
+  end
+end
